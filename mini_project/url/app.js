4c419afdb9ef80a2a058e8d16c871a9bef35cc8e
Faraz <SP24BSCS0044@maju.edu.pk>
2025-10-02 15:14:34 +0500

    Node js

--- mini_project/url/app.js
+++ mini_project/url/app.js
@@ -1,27 +1,30 @@
-import { readFile } from "fs";
+import { readFile } from "fs/promises";
 import { createServer } from "http";
-import path from "path/posix";
+import path from "path";
 
 const PORT = 3002;
-const server = createServer( async (req, res) => {
-if(req.method === "GET"){
-    if(req.url === "/"){
-        try{
-        const data = await readFile(path.join("public", "index.html"));
-        res.writeHead(200, { "Content-Type" : "text/html"});
-        res.end(data);
+
+const serverFile = async (res, filePath, contentType) => {
+  try {
+    const data = await readFile(filePath);
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(data);
+  } catch (error) {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 page not found");
+  }
+};
+
+const server = createServer(async (req, res) => {
+  if (req.method === "GET") {
+    if (req.url === "/") {
+      return serverFile(res, path.join("public", "index.html"), "text/html");
+    } else if (req.url === "/style.css") {
+      return serverFile(res, path.join("public", "style.css"), "text/css");
     }
-        catch (error) {
-            res.writeHead(404, { "Content-Type" : "text/html"});
-            res.end("404 page not found");
-        }
-    }
+  }
+});
 
-}
-})
-
-
-server.listen(PORT, ()=> {
- console.log(`Server running at http://localhost:${PORT}`);
-} )
-
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
