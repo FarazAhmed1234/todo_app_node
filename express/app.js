0acc67bd3d341cc6416ce4cf12b6521a9f746a5a
Faraz <SP24BSCS0044@maju.edu.pk>
2025-10-03 17:54:43 +0500

    Node js

--- express/app.js
+++ express/app.js
@@ -1,14 +1,23 @@
-import express from 'express';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Faraz Ahmed!") );
+// Fix for __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// Serve static files (CSS, JS, images) from public folder
+app.use(express.static("public"));
 
-// $env:PORT=3000; node app.js   ----->>> this is line to call to use this is is use to 
-const PORT = process.env.PORT;
+app.get("/", (req, res) => {
+  const homePagePath = path.join(__dirname, "public", "index.html");
+  res.sendFile(homePagePath);
+});
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () =>{
-    console.log(`Server is running at port: ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running at port: ${PORT}`);
+});
